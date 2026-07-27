0b7c98028d5c2c3b3aeafd3a690085337db4c8e0
ephraimphrase <ephraimphrase@gmail.com>
2026-07-27 18:44:56 +0100

    questionnaire overhaul

--- waitlist/views.py
+++ waitlist/views.py
@@ -13,11 +13,20 @@ def index(request):
                 name=data.get('name'),
                 business_name=data.get('business'),
                 contact=data.get('whatsapp'),
-                platform=data.get('platform'),
+                product_category=data.get('product_category'),
+                business_duration=data.get('business_duration'),
                 order_volume=data.get('order_volume'),
-                sales_channels=data.get('sales_channels'),
-                delivery_method=data.get('delivery_method'),
-                ideal_software=data.get('ideal_software')
+                sales_channels=data.get('sales_channels', []),
+                current_tools=data.get('current_tools'),
+                time_drains=data.get('time_drains', []),
+                frustrations=data.get('frustrations', []),
+                lost_money=data.get('lost_money'),
+                lost_money_details=data.get('lost_money_details'),
+                desired_automation=data.get('desired_automation'),
+                worst_task=data.get('worst_task'),
+                two_hours_saved=data.get('two_hours_saved'),
+                interest_level=data.get('interest_level'),
+                biggest_slowdown=data.get('biggest_slowdown')
             )
             return JsonResponse({'status': 'success'})
         except Exception as e:
